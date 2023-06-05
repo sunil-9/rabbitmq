5fb24d38d3b6d67d10528bfe6b4608ae389f4cc2
sunil-9 <suneelsapkota9@gmail.com>
2023-06-05 13:17:50 +0545

    chore: settng env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // stand alone true for serverless
+  
+  output: "standalone",
+  env: {
+    DB_URL: `mongodb+srv://xxx:xxx@xxx.xxx.mongodb.net/xxx`,
+    RABBIT_MQ_USER: "guest",
+    RABBIT_MQ_PASSWORD: "admin",
+    RABBIT_MQ_HOST: "xx.xx.xx.xx",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
